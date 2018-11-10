91ae2c31d91273b519c5821439a277e237aedb66
GauthamMaraswami <15co118.gautham@nitk.edu.in>
2018-11-10 12:59:26 +0530

    linear regression and group mean added

--- dwm/normalization.py
+++ dwm/normalization.py
@@ -5,7 +5,31 @@
  
 # Normalize time series data
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+def mean(data):
+    """Return the sample arithmetic mean of data."""
+    n = len(data)
+    if n < 1:
+        raise ValueError('mean requires at least one data point')
+    return sum(data)/n # in Python 2 use sum(data)/float(n)
+
+def _ss(data):
+    """Return sum of square deviations of sequence data."""
+    c = mean(data)
+    ss = sum((x-c)**2 for x in data)
+    return ss
+
+def stddev(data, ddof=0):
+    """Calculates the population standard deviation
+    by default; specify ddof=1 to compute the sample
+    standard deviation."""
+    n = len(data)
+    if n < 2:
+        raise ValueError('variance requires at least two data points')
+    ss = _ss(data)
+    pvar = ss/(n-ddof)
+    return pvar**0.5
 
 def normalize(array,req_min,req_max):
     curr_min=min(array)
@@ -15,10 +39,24 @@ def normalize(array,req_min,req_max):
         array[i]=req_min+((array[i]-curr_min)*(req_max-req_min)/(curr_max-curr_min))
     return array
 
+def zScore(array):
+    arrmean=mean(array)
+    stddevval=stddev(array)
+    for i in range(0,len(array)):
+        array[i]=(array[i]-arrmean)/stddevval
+    return array
 
+def decimalNormalization(array):
+    maxele=max(array)
+    i = 1;
+    while((i * 10) < maxele):
+        i *= 10;
+    for i1 in range(0,len(array)):
+        array[i1]=(array[i1]/(i*10));
+    return array
 
 series = pd.read_csv('pima.csv', header=None)
 fixerrange=pd.to_numeric(series[3],errors='coerce').astype(float)
-print(fixerrange)
-fixerrange=normalize(fixerrange,0,1)
-print(fixerrange)
+print max(fixerrange)
+fixerrange=decimalNormalization(fixerrange)
+print max(fixerrange)
--- dwm/test.py
+++ dwm/test.py
@@ -1,6 +1,46 @@
 from pandas import read_csv,to_numeric
-import numpy
+import pandas as pd
+import numpy as np
+from scipy.stats import stats
+def mean(data):
+    """Return the sample arithmetic mean of data."""
+    n = len(data)
+    if n < 1:
+        raise ValueError('mean requires at least one data point')
+    return sum(data)/n # in Python 2 use sum(data)/float(n)
 
+def _ss(data):
+    """Return sum of square deviations of sequence data."""
+    c = mean(data)
+    ss = sum((x-c)**2 for x in data)
+    return ss
+
+def stddev(data, ddof=0):
+    """Calculates the population standard deviation
+    by default; specify ddof=1 to compute the sample
+    standard deviation."""
+    n = len(data)
+    if n < 2:
+        raise ValueError('variance requires at least two data points')
+    ss = _ss(data)
+    pvar = ss/(n-ddof)
+    return pvar**0.5
+
+def zScore(array):
+    arrmean=mean(array)
+    stddevval=stddev(array)
+    for i in range(0,len(array)):
+        array[i]=(array[i]-arrmean)/stddevval
+    return array
+
+def decimalNormalization(array):
+    maxele=max(array)
+    i = 1;
+    while((i * 10) < maxele):
+        i *= 10;
+    for i1 in range(0,len(array)):
+        array[i1]=(array[i1]/(i*10));
+    return array
 def normalize(array,req_min,req_max):
     curr_min=min(array)
     curr_max=max(array)
@@ -9,7 +49,38 @@ def normalize(array,req_min,req_max):
     print(array.head())
     return array
 
-def operate(choice,dataset):
+def fillByLinearRegression(array,x,y):
+    initlen=array[x].count()
+    dataset1=array.dropna(subset=[y])
+    dataset1=dataset1.dropna(subset=[x])
+   # df1=pd.DataFrame({'X': dataset1[x], 'Y': dataset1[y]}, index=index)
+    from sklearn import linear_model
+    regr=linear_model.LinearRegression()
+    len1=dataset1[x].count()
+    print(len1)
+    print (initlen)
+    la=np.array(dataset1[y]).reshape(len1,1)
+    ai=np.array(dataset1[x]).reshape(len1,1)
+    mod1=regr.fit(ai,la)
+    Rsquare=regr.score(ai,la)
+    Intercept=regr.intercept_
+    Coeff=regr.coef_
+    print("Rsquare:%f" % Rsquare)
+    print("Intercept:%f" % Intercept)
+    print("Coeff:%f" % Coeff)
+    test=array[x][array[y].isnull()]
+    predict=regr.predict(test.values.reshape((initlen-len1),1))
+    test.replace(to_replace=test.values,value=predict,inplace=True)
+    array[y].fillna(value=test,inplace=True)
+    return array
+
+
+
+
+
+
+
+def operate(choice,dataset,col_list):
     if choice=='d':
         dataset.dropna(inplace=True)
     elif choice=='me':
@@ -22,13 +93,23 @@ def operate(choice,dataset):
         print('Enter the number to fill the empty spaces')
         const=float(raw_input())
         dataset.fillna(const, inplace=True)
+    elif choice=='g':
+        grouping_attr = raw_input('Enter the Grouping Attribute :')
+        for col in col_list:
+            dataset[col] = dataset.groupby([int(grouping_attr)])[col]\
+                .transform(lambda x: x.fillna(x.mean()))
+    elif choice=='lr':
+        X_attr = raw_input('Enter the X Attribute :')
+        Y_attr = raw_input('Enter the Y Attribute :')
+        dataset=fillByLinearRegression(dataset,int(X_attr),int(Y_attr))
     else:
         raise ValueError('Invalid Input')
     return dataset
 
 def replace(dataset,col_list,choice):
-    dataset[col_list] = dataset[col_list].replace(0, numpy.NaN)
-    dataset=operate(choice,dataset)
+    dataset[col_list] = dataset[col_list].replace(0, np.NaN)
+    dataset.to_csv('I.csv', sep='\t')
+    dataset=operate(choice,dataset,col_list)
     return dataset
 
 print ('Enter the file name')
@@ -51,11 +132,25 @@ while(ch=='y'):
 print('do you want to normalize')
 ch=raw_input()
 while(ch=='y'):
-    print('enter the column number min and max values')
-    col,minval,maxval=raw_input().split(' ')
-    dataset[int(col)]=to_numeric(dataset[int(col)],errors='coerce').astype(float)
-    dataset[int(col)]=normalize(dataset[int(col)],float(minval),float(maxval))
-    print('do you want to normalize')
+    print('enter type of normalization 1.min/max 2.zscore 3.DecimalScale')
+    option=raw_input()
+    if option=='1':
+        print('enter the column number min and max values')
+        col,minval,maxval=raw_input().split(' ')
+        dataset[int(col)]=to_numeric(dataset[int(col)],errors='coerce').astype(float)
+        dataset[int(col)]=normalize(dataset[int(col)],float(minval),float(maxval))
+    elif option=='2':
+        col=raw_input()
+        dataset[int(col)]=to_numeric(dataset[int(col)],errors='coerce').astype(float)
+        dataset[int(col)]=zScore(dataset[int(col)])
+    elif option=='3':
+        col=raw_input()
+        dataset[int(col)]=to_numeric(dataset[int(col)],errors='coerce').astype(float)
+        dataset[int(col)]=decimalNormalization(dataset[int(col)])
+    else:
+        continue
+    print('do you want to normalize again')
     ch=raw_input()
 dataset.dropna(inplace=True)
 print(dataset.head())
+dataset.to_csv('O.csv', sep='\t')
